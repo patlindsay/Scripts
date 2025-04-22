1091cd4b719413db2215966b5c9d8d07256fa4c3
patlindsay <129234245+patlindsay@users.noreply.github.com>
2025-04-21 19:58:49 -0500

    Initial commit
    This script is to quickly uninstall all Astro PNP Audio devices. This is described in the top comment here: https://www.reddit.com/r/AstroGaming/comments/ko2yc4/a20_gen_2_wireless_headset_stuck_in_playstation/

---
+++ DeleteAstroDevices.ps1
@@ -0,0 +1,13 @@
+﻿$astroDevices = Get-PnpDevice | Where-Object {($_.Name -match "^Astro*")}
+
+Write-Host $astroDevices
+
+Disable-PnpDevice $astroDevices.DeviceId -Confirm -WhatIf
+
+for each
+
+$result = Get-PnpDevice | Where-Object {($_.PNPDeviceID == $astroDevices
+
+if ($result )
+
+Write-Host $result
